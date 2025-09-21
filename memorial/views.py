1c6dc978045ceaf4d8c9d44499cbe404f7632c8c
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-21 19:25:20 +0100

    memorial edit view

--- memorial/views.py
+++ memorial/views.py
@@ -7,6 +7,9 @@ Handles memorial creation, management, and all related functionality.
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, reverse
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 # Local Apps
@@ -39,3 +42,34 @@ class MemorialCreateView(LoginRequiredMixin, CreateView):
             'plans:choose_plan',
             kwargs={'memorial_id': memorial.pk}
         ))
+
+
+class MemorialEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """View for editing existing memorials"""
+    model = Memorial
+    form_class = MemorialForm
+    template_name = 'memorials/memorial_edit.html'
+
+    def get_success_url(self):
+        """Redirect to memorial detail after edit"""
+        return reverse_lazy(
+            'memorials:memorial_detail',
+            kwargs={'pk': self.object.pk}
+        )
+
+    def test_func(self):
+        """Ensure only memorial owner can edit"""
+        return self.request.user == self.get_object().user
+
+    def get_context_data(self, **kwargs):
+        """Add update URLs to context"""
+        context = super().get_context_data(**kwargs)
+        context['update_name_url'] = reverse_lazy(
+            'memorials:update_name',
+            kwargs={'pk': self.object.pk}
+        )
+        context['update_dates_url'] = reverse_lazy(
+            'memorials:update_dates',
+            kwargs={'pk': self.object.pk}
+        )
+        return context
