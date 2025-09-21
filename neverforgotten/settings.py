5163d4e37458357c233aa79693db8a1bbb1b9fc3
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-21 19:01:24 +0100

    email settings added

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -160,6 +160,19 @@ cloudinary.config(
     secure=True
 )
 
+# ========================
+# Email Configuration
+# ========================
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_PORT = config('EMAIL_PORT', cast=int)
+    EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
