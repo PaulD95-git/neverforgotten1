8823ae5438cbb18fa66a6bc2c8f8cc52e683770c
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-21 18:57:53 +0100

    middleware updated

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -52,12 +52,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'neverforgotten.urls'
