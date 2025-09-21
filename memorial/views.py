417fb70b89fba930cf2f3cefc04d7022779dac0b
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-21 19:07:54 +0100

    Create memorial view created

--- memorial/views.py
+++ memorial/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+"""
+Views for NeverForgotten application.
+Handles memorial creation, management, and all related functionality.
+"""
 
-# Create your views here.
+# Django Core
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, reverse
+from django.views.generic import CreateView
+
+
+# Local Apps
+from .forms import MemorialForm
+from .models import Memorial
+
+# ---------------------------
+# Memorial CRUD Views
+# ---------------------------
+
+
+class MemorialCreateView(LoginRequiredMixin, CreateView):
+    """View for creating new memorials"""
+    model = Memorial
+    form_class = MemorialForm
+    template_name = 'memorials/memorial_form.html'
+
+    def form_valid(self, form):
+        """Assign user and default free plan before saving"""
+        form.instance.user = self.request.user
+
+        try:
+            free_plan = Plan.objects.get(name='free')
+            form.instance.plan = free_plan
+        except Plan.DoesNotExist:
+            pass
+
+        memorial = form.save()
+        return redirect(reverse(
+            'plans:choose_plan',
+            kwargs={'memorial_id': memorial.pk}
+        ))
