6a160ab12c40df77670d527ea612370affcd7633
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-25 15:01:04 +0100

    GalleryImageInline

--- memorial/admin.py
+++ memorial/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Memorial, Tribute, GalleryImage, ContactMessage
 
-# Register your models here.
+# --- Admin Registrations ---
+admin.site.register(Tribute)  # Basic registration (no custom admin needed)
+
+
+# --- Inline Admin Classes ---
+class GalleryImageInline(admin.TabularInline):
+    """Inline editor for memorial gallery images."""
+    model = GalleryImage
+    extra = 1  # Number of empty image slots shown by default
+
+
