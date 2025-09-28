bf06c7d02fa44731e21294c79cebfde8b5c0cfcb
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-28 11:47:34 +0100

    removed cloudinary config call

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -166,12 +166,6 @@ CLOUDINARY_STORAGE = {
     'SECURE': True
 }
 
-cloudinary.config(
-    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
-    api_key=config('CLOUDINARY_API_KEY'),
-    api_secret=config('CLOUDINARY_API_SECRET'),
-    secure=True
-)
 
 # ========================
 # Email Configuration
