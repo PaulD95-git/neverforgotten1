82dc0f523631e3b21deed86d2459d93d844021f1
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-09-28 11:38:41 +0100

    STATICFILES_STORAGE changed

--- neverforgotten/settings.py
+++ neverforgotten/settings.py
@@ -152,7 +152,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
