c01b3ce638beeebc9f5681d13019616214e6a843
PaulD95-git <r39ij6ua@students.codeinstitute.net>
2025-11-22 16:43:39 +0000

    feat: add sitemap.xml with URL route and static robots.txt for SEO

--- neverforgotten/urls.py
+++ neverforgotten/urls.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 
-
+    path('sitemap.xml', TemplateView.as_view(
+        template_name='sitemap.xml', 
+        content_type='application/xml'
+    )),
 
     path('', include('memorial.urls')),
 
